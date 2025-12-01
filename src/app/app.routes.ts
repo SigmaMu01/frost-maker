d524a230b3f4132a8b35520bd325178f14a39f68
SigmaMu01 <egor.maksimov.d@gmail.com>
2025-12-01 06:40:37 +0500

    add dummy temperature panel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Grid } from './panels/grid/grid';
+import { Intro } from './panels/intro/intro';
+import { Viewport } from './panels/viewport/viewport';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Intro,
+  },
+  {
+    path: 'grid',
+    component: Grid,
+  },
+  {
+    path: 'viewport',
+    component: Viewport,
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidenav } from './components/sidenav/sidenav';
+import { Temperature } from './panels/temperature/temperature';
+import { Timeline } from './panels/timeline/timeline';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Sidenav, Temperature, Timeline],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('frost-maker');
